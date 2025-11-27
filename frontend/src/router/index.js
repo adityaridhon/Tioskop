cd3967e957f98f8ca068da10cbaa02c9958dd458
alyxph <11231007@student.itk.ac.id>
2025-11-27 14:16:45 +0800

    penambahan dasboard admin

---
+++ frontend/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/Home.vue'
+import Admin from '../pages/Admin.vue'
+import JadwalTayang from '../pages/JadwalTayang.vue'
+import Movies from '../pages/Movies.vue'
+import Users from '../pages/Users.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/admin/jadwaltayang',
+    name: 'Schedule',
+    component: JadwalTayang
+  },
+  {
+    path: '/admin/movies',
+    name: 'Movies',
+    component: Movies
+  },
+  {
+    path: '/admin/users',
+    name: 'Users',
+    component: Users
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
